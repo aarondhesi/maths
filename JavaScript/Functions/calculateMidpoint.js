31cfa5828112a42222c4889404f0be22c6b44a1c
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-04-05 21:35:51 +0100

    Add calculateMidpoint() function
    Calculates midpoint of two numbers

---
+++ JavaScript/Functions/calculateMidpoint.js
@@ -0,0 +1,10 @@
+/**
+ * Calculates midpoint of num1 and num2
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns {Number} midpoint of num1 and num2
+ */
+
+const calculateMidpoint = (num1, num2) => (num1 + num2) / 2;
+
+export { calculateMidpoint };
