127869a08fb58f1b2e79a0009240ec2d6588236b
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-04-05 21:33:47 +0100

    Rename function
    - getBool --> getRandomBool()

--- JavaScript/Functions/getBool.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns true or false
- * @returns {boolean} true or false
- */
-
-const getBool = () => Math.random() < 0.5;
-
-export { getBool };
---
+++ JavaScript/Functions/getRandomBoolean.js
@@ -0,0 +1,8 @@
+/**
+ * Returns true or false
+ * @returns {boolean} true or false
+ */
+
+const getRandomBoolean = () => Math.random() < 0.5;
+
+export { getRandomBoolean };
