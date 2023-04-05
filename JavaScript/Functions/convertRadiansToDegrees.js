435cd0e7f3790a45b6eb7c31f4c5678e91d3623c
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-04-05 21:37:47 +0100

    Add convertRadiansToDegrees() function
    - Converts radians to degrees

---
+++ JavaScript/Functions/convertRadiansToDegrees.js
@@ -0,0 +1,9 @@
+/**
+ * Converts radians to degrees
+ * @param {number} angle angle in radians
+ * @returns {number} angle in degrees
+ */
+
+ const convertRadiansToDegrees = (angle) => angle * 180 / Math.PI;
+
+ export { convertRadiansToDegrees };
